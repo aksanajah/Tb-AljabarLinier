b41f27518ef9681b45be32bb14c4afe78581c4ea
aksanajah <120318379+aksanajah@users.noreply.github.com>
2023-06-23 09:11:18 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,14 @@
+# biodata mahasiswa
+
+print(" data diri mahasiswa")
+
+Nama = input("Masukan Nama : ")
+Nim = input("Masukan Nim : ")
+Umur = input("Masukan Umur : ")
+Motivasi = input("Masukan Motivasi : ")
+
+print("\n\n\nnama  :",nama)
+print("nim     :",nim)
+print("umur   :",umur)
+
+print('\n\t\t karena jurusan ini memiliki prospek kerja yang bagus')
---
+++ nama.py
@@ -0,0 +1,11 @@
+nama = None
+while not nama:
+ nama = input("Nama: ")
+umur = int(input("Umur: "))
+nim = int(input("NIM: "))
+tanggal_lahir = input("Tahun kelahiran: ")
+
+print("Hello "+nama)
+print("Umur kamu "+str(umur)+" tahun")
+print("NIM kamu "+str (nim))
+print("Tahun kelahiran kamu "+str(tahun_kelahiran))
